b50ad8059a8c8baa23686dbfa93c56a7df6e8f74
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-27 16:09:37 -0300

    test: mustReturnListWithAllDrones

--- src/test/java/com/dronefeeder/DroneFeederApplicationTests.java
+++ src/test/java/com/dronefeeder/DroneFeederApplicationTests.java
@@ -1,13 +1,48 @@
 package com.dronefeeder;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class DroneFeederApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mockMvc;
+
+  @SpyBean
+  private DroneRepository droneRepository;
+
+  @Test
+   void mustReturnListWithAllDrones() throws Exception {
+    final var result = mockMvc.perform(get("/dronefeeder/all"));
+
+    final var droneOne = new DroneFeeder("Heygelo", "S90", "123456");
+    final var droneTwo = new DroneFeeder("Deerc", "D20", "654321");
+    final var droneThree = new DroneFeeder("Neheme", "NH760", "123987");
+  
+    droneRepository.save(droneOne);
+    droneRepository.save(droneTwo);
+    droneRepository.save(droneThree);
+   
+     result
+       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+       .andExpect(status().isOk())
+       .andExpect(jsonPath("$[0].brand").value(droneOne.getBrand()));
+       .andExpect(jsonPath("$[0].modelName").value(droneOne.getModelName()));
+       .andExpect(jsonPath("$[0].serialNumber").value(droneOne.getSerialNumber()));
+       .andExpect(jsonPath("$[2].brand").value(droneThree.getBrand()));
+       .andExpect(jsonPath("$[2].modelName").value(droneThree.getModelName()));
+       .andExpect(jsonPath("$[2].serialNumber").value(droneThree.getSerialNumber()));
+   }
 
 }
