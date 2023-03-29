4c543c0b3c2ef6287e074254c34c57617e07591e
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-03-29 13:34:34 -0300

    Fix: corrects checkstyle problems

--- src/main/java/com/dronefeeder/DroneFeederApplication.java
+++ src/main/java/com/dronefeeder/DroneFeederApplication.java
@@ -3,11 +3,14 @@ package com.dronefeeder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DroneFeederApplication class.
+ */
 @SpringBootApplication
 public class DroneFeederApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DroneFeederApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DroneFeederApplication.class, args);
+  }
 
 }
