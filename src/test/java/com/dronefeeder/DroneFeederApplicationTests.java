e362295b5f36b270c7410c420edea2339cbb8d9b
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-29 19:04:53 -0300

    fix: path of mustReturnListWithAllDrones

--- src/test/java/com/dronefeeder/DroneFeederApplicationTests.java
+++ src/test/java/com/dronefeeder/DroneFeederApplicationTests.java
@@ -26,28 +26,26 @@ class DroneFeederApplicationTests {
 
   @SpyBean
   private DroneRepository droneRepository;
-  
-  @BeforeEach
-  public void setUp() {
-    droneRepository.deleteAll();
-    this.droneRepository = new DroneRepository()
-  }
+
+  // @BeforeEach
+  // public void setUp() {
+  // droneRepository.deleteAll();
+  //// this.droneRepository = new DroneRepository()
+  // }
 
   @Test
-   void mustReturnListWithAllDrones() throws Exception {
-    final var result = mockMvc.perform(get("/dronefeeder/drone/all"));
+  void mustReturnListWithAllDrones() throws Exception {
+    final var result = mockMvc.perform(get("/dronefeeder/drone/"));
 
     final var droneOne = new DroneFeeder("Heygelo", "S90", "123456");
     final var droneTwo = new DroneFeeder("Deerc", "D20", "654321");
     final var droneThree = new DroneFeeder("Neheme", "NH760", "123987");
-  
+
     droneRepository.save(droneOne);
     droneRepository.save(droneTwo);
     droneRepository.save(droneThree);
-   
-    result
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
+
+    result.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
         .andExpect(jsonPath("$[0].brand").value(droneOne.getBrand()))
         .andExpect(jsonPath("$[0].modelName").value(droneOne.getModelName()))
         .andExpect(jsonPath("$[0].serialNumber").value(droneOne.getSerialNumber()))
@@ -62,72 +60,66 @@ class DroneFeederApplicationTests {
     droneRepository.save(droneOne);
 
     final ResultActions result = mockMvc.perform(get("/dronefeeder/drone/" + droneOne.getId()));
- 
-    result
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
+
+    result.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
         .andExpect(jsonPath("$.brand").value(droneOne.getBrand()))
         .andExpect(jsonPath("$.modelName").value(droneOne.getModelName()))
         .andExpect(jsonPath("$.serialNumber").value(droneOne.getSerialNumber()));
   }
-  
+
   @Test
   void mustAddNewDrone() throws Exception {
     final var droneOne = new DroneFeeder("Heygelo", "S90", "123456");
 
     final ResultActions result = mockMvc.perform(post("/dronefeeder/drone/"));
- 
+
     result
         .andExpect(content(new ObjectMapper().writeValueAsString(droneOne))
             .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.brand").value(droneOne.getBrand()))
+        .andExpect(status().isCreated()).andExpect(jsonPath("$.brand").value(droneOne.getBrand()))
         .andExpect(jsonPath("$.modelName").value(droneOne.getModelName()))
         .andExpect(jsonPath("$.serialNumber").value(droneOne.getSerialNumber()));
   }
-  
+
   @Test
   void mustThrowErrorIfDroneAlreadyExists() throws Exception {
     final var droneOne = new DroneFeeder("Heygelo", "S90", "123456");
     droneRepository.save(droneOne);
-    
+
     final ResultActions result = mockMvc.perform(post("/dronefeeder/drone/"));
- 
+
     result
         .andExpect(content(new ObjectMapper().writeValueAsString(droneOne))
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isConflict())
         .andExpect(jsonPath("$.message").value("Drone is already registered!"));
   }
-  
+
   @Test
   void mustDeleteTheDroneById() throws Exception {
     final var droneOne = new DroneFeeder("Heygelo", "S90", "123456");
     droneRepository.save(droneOne);
 
     final var result = mockMvc.perform(delete("/dronefeeder/drone/" + droneOne.getId()));
- 
-    result
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+
+    result.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
   }
-  
+
   @Test
   void mustUpdateTheDroneById() throws Exception {
     final var droneOne = new DroneFeeder("Heygelo", "S90", "123456");
     final var droneOneUpdated = new DroneFeeder("Heygelo", "S91", "123456");
-    
+
     droneRepository.save(droneOne);
 
     mockMvc.perform(get("/dronefeeder/drone/" + droneOne.getId()))
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.modelName").value(droneOne.getModelName()));
-    
-    final ResultActions result = mockMvc.perform(put("/dronefeeder/drone/" + droneOneUpdated.getId()));
-    
-    result
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
+
+    final ResultActions result =
+        mockMvc.perform(put("/dronefeeder/drone/" + droneOneUpdated.getId()));
+
+    result.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
         .andExpect(jsonPath("$.modelName").value(droneOneUpdated.getModelName()));
   }
 }
