c65b720d77c9e4764472e4d8175ea7629e3dd558
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-30 17:21:41 -0300

    fix: refactor build application

--- src/main/java/com/dronefeeder/model/DroneFeeder.java
+++ src/main/java/com/dronefeeder/model/DroneFeeder.java
@@ -2,12 +2,12 @@ package com.dronefeeder.model;
 
 
 //import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 //import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import javax.persistence.Table;
 //import java.util.List;
 
 /**
