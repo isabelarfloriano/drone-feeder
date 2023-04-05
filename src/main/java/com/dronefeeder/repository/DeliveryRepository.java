8de327f2f50c57bf6b94cb8457b6de3743bb5cd9
vanessabastos <vanessarsb83@gmail.com>
2023-04-05 18:24:21 -0300

    Fix: Corrects Delivery Repository

--- src/main/java/com/dronefeeder/repository/DeliveryRepository.java
+++ src/main/java/com/dronefeeder/repository/DeliveryRepository.java
@@ -1,13 +1,12 @@
 package com.dronefeeder.repository;
 
+import com.dronefeeder.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.dronefeeder.model.Delivery;
+
 
 /**
  * DeliveryRepository.
  */
 @Repository
-public interface DeliveryRepository extends JpaRepository<Delivery, Long>{
-  
-}
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {}
