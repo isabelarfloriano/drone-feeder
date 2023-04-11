5ee1e4c1c00798f0077e508def81592b090519a8
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-11 18:28:35 -0300

    fix: imports

--- src/test/java/com/dronefeeder/DeliveryTests.java
+++ src/test/java/com/dronefeeder/DeliveryTests.java
@@ -24,7 +24,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.cache.support.NullValue;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
