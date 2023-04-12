0f61dd074456f0714902cc91338213763221338f
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-04-12 07:36:01 -0300

    Fix: corrects lint file

--- src/main/java/com/dronefeeder/swagger/SwaggerConfiguration.java
+++ src/main/java/com/dronefeeder/swagger/SwaggerConfiguration.java
@@ -2,7 +2,6 @@ package com.dronefeeder.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +9,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 // swagger configuration by larissa falcão at online class;
 // url: http://localhost:8080/swagger-ui/index.html
-
+/**
+ * SwaggerConfiguration class.
+ */
 @Configuration
 public class SwaggerConfiguration {
   /**
