0d04f411ecb7ac47fa41de66470b8c646857c28d
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-04-18 09:43:13 -0300

    Fix: comments swagger

--- src/main/java/com/dronefeeder/swagger/SwaggerConfiguration.java
+++ src/main/java/com/dronefeeder/swagger/SwaggerConfiguration.java
@@ -1,28 +1,27 @@
 package com.dronefeeder.swagger;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-// swagger configuration by larissa falcão at online class;
-// url: http://localhost:8080/swagger-ui/index.html
-/**
- * SwaggerConfiguration class.
+/*
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration; import
+ * springfox.documentation.builders.PathSelectors; import
+ * springfox.documentation.builders.RequestHandlerSelectors; import
+ * springfox.documentation.spi.DocumentationType; import
+ * springfox.documentation.spring.web.plugins.Docket;
+ * 
+ * // swagger configuration by larissa falcão at online class;
+ * // url: http://localhost:8080/swagger-ui/index.html
+ *
+*
+ * @Configuration
+ * public class SwaggerConfiguration {
+ *
+ *   @Bean
+ *   public Docket docket() {
+ *     return new Docket(DocumentationType.SWAGGER_2)
+ *         .select()
+ *         .apis(RequestHandlerSelectors.any())
+ *         .paths(PathSelectors.any())
+ *         .build();
+ *   }
+ * }
  */
-@Configuration
-public class SwaggerConfiguration {
-  /**
-   * Docket.
-   */
-  @Bean
-  public Docket docket() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build();
-  }
-}
